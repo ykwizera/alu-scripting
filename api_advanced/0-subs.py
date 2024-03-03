ec1be1066c6e3e1fc6ede83b15e9c85ae7ab5148
ykwizera <y.kwizera@alustudent.com>
2024-03-03 10:37:56 -0800

    0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,27 +4,17 @@ Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
+import praw
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If not a valid subreddit, returns 0.
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'my-reddit-api-client'}
-    
+    """Return the total number of subscribers on a given subreddit."""
+    reddit = praw.Reddit(client_id='my_client_id',
+                           client_secret='my_client_secret',
+                           user_agent='my_user_agent')
+
     try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        
-        data = response.json()
-        
-        if data['kind'] == 't5':
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.exceptions.HTTPError as err:
-        if response.status_code == 404:
-            return 0
-        else:
-            raise err
+        subreddit_data = reddit.subreddit(subreddit)
+        subscribers = subreddit_data.subscribers
+        return subscribers
+    except praw.exceptions.NotFound:
+        return 0
