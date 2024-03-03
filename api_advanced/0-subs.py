69660ff5c4ccdcc348720498cc8c8db44247dc16
ykwizera <y.kwizera@alustudent.com>
2024-03-03 10:33:17 -0800

    ok

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -14,6 +14,7 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
+        print("OK")
         return subscribers
     else:
         return 0
